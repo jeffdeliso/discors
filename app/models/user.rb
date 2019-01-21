8e1476ae75a591be8c3b295d2363686fb6f7a79a
Jeff DeLiso <jeff.deliso@gmail.com>
2019-01-21 13:01:27 -0500

    Run users migration

---
+++ app/models/user.rb
@@ -0,0 +1,65 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  image_url       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+
+  attr_reader :password
+
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.generate_unique_session_token unless self.session_token
+  end
+
+  def new_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def generate_unique_session_token
+    self.session_token = self.new_session_token
+    while User.find_by(session_token: self.session_token)
+      self.session_token = self.new_session_token
+    end
+    self.session_token
+  end
+
+end
