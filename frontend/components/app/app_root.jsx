05f9cda235b22bcd437291a2d2aa68bb60ab9cc6
Jeff DeLiso <jeff.deliso@gmail.com>
2019-01-23 11:23:37 -0500

    Added router for main app

--- frontend/components/app/app_root.jsx
+++ frontend/components/app/app_root.jsx
@@ -1,34 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Servers from './servers/servers_container';
 import Channels from './channels/channels_container';
 import Header from './header/header';
 import Chat from './chat/chat';
 import ServerMembers from './server_members/server_members';
+import ServerRoute from './app_routes/server_route';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+import MeRoute from './app_routes/me_route';
 
 class AppRoot extends React.Component {
   componentDidMount() {
-    document.body.style="overflow: hidden;";
+    document.body.style = "overflow: hidden;";
   }
 
   componentWillUnmount() {
-    document.body.removeAttribute("style")
+    document.body.removeAttribute("style");
   }
 
   render() {
     return (
       <div className="main-body">
         <Servers />
-        <div className="right-main">
-          <Channels />
-        </div>
-        <div className="chat-container">
-          <Header />
-          <div className="chat-main-container">
-            <Chat />
-            <ServerMembers />
-          </div>
-        </div>
+        <Switch >
+          <Route path="/channels/@me" component={MeRoute} />
+          <Route path="/channels/:serverId/:channelId" component={ServerRoute} />
+          <Route path="/channels" component={MeRoute} />
+        </Switch >
       </div>
     )
   }
---
+++ frontend/components/app/app_routes/me_route.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Channels from '../channels/channels_container';
+import Header from '../header/header';
+import Chat from '../chat/chat';
+import ServerMembers from '../server_members/server_members';
+
+class MeRoute extends React.Component {
+
+  render() {
+    return (
+      <>
+        <Channels />
+        <div className="chat-container">
+          <Header />
+          <div className="chat-main-container">
+            <Chat />
+            <ServerMembers />
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+
+export default MeRoute;
