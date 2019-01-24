f101b7d679ab767c83ef085b38e3dbcb517fbca8
Jeff DeLiso <jeff.deliso@gmail.com>
2019-01-24 10:55:37 -0500

    Change to scss

--- frontend/components/app/server_members/server_member.jsx
+++ frontend/components/app/server_members/server_member.jsx
@@ -6,7 +6,7 @@ class ServerMember extends React.Component {
   render() {
     return (
       <div className="server-member">
-        <div className="server-member-img" style={this.props.user.image_url ? {backgroundImage: `url(${this.props.user.image_url})`} : {}}></div>
+        <div className="server-member-img" style={this.props.user.image_url ? {backgroundImage: `image-url(${this.props.user.image_url})`} : {}}></div>
         <span>{this.props.user.username}</span>
       </div>
     )
