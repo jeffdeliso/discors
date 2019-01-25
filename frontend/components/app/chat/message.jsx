2ab383fc60b2afe6f04270cd3358bbd563acc7ca
Jeff DeLiso <jeff.deliso@gmail.com>
2019-01-25 00:05:38 -0500

    Add date to messages

--- frontend/components/app/chat/message.jsx
+++ frontend/components/app/chat/message.jsx
@@ -5,6 +5,12 @@ class Message extends React.Component {
     super(props);
   }
 
+  parseDate() {
+    debugger
+    const dateArr = this.props.message.created_at.split('T')[0].split('-');
+    return `${dateArr[1]}/${dateArr[2]}/${dateArr[0]}.`;
+  }
+
   render() {
     return (
       <div className="message">
@@ -12,7 +18,7 @@ class Message extends React.Component {
         <div className="username-message">
           <div className="username-time">
             <h4>{this.props.user.username}</h4>
-            <h5>Today</h5>
+            <h5>{this.parseDate()}</h5>
           </div>
           <span>{this.props.message.body}</span>
         </div>
