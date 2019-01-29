d2d365095ff6c1f66ad9227d434e7ed209a69807
Jeff DeLiso <jeff.deliso@gmail.com>
2019-01-29 09:48:44 -0500

    Change config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-  
+  config.action_cable.url = 'wss://yourapp.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ '*' ]
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
