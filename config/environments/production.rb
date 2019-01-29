ed5032c168e327b46921be01d3831bd5a4523470
Jeff DeLiso <jeff.deliso@gmail.com>
2019-01-29 09:45:14 -0500

    Change config for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,8 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  
+  config.action_cable.allowed_request_origins = [ '*' ]
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
