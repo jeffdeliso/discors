996f0d0087dda4cb16a47e6399d9b98c0f585d60
Jeff DeLiso <jeff.deliso@gmail.com>
2019-02-03 18:41:58 -0500

    Add notification transition

--- frontend/components/app/servers/dm_notification.jsx
+++ frontend/components/app/servers/dm_notification.jsx
@@ -3,13 +3,20 @@ import { Link } from 'react-router-dom';
 import Tooltip from '../modal/tooltip';
 
 class DmNotification extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { classText: 'dm-notification'};
+  }
 
+  componentDidMount() {
+    setTimeout(() => this.setState({classText: 'dm-notification right'}), 1);
+  }
   render() {
     return (
       <Tooltip component={
         <Link to={`/channels/@me/${this.props.notification.channelId}`}
-          className="home-icon server"
-          id="dm-notification"
+          className={`home-icon ${this.state.classText}`}
+          id={this.state.classText}
           style={{ backgroundImage: `url(${this.props.user.image_url})`, backgroundSize: '100%'}}
           onKeyDown={(e) => e.preventDefault()}
         >
