55f8ad0c9025af015f767d16198d142a7399d53a
Jeff DeLiso <jeff.deliso@gmail.com>
2019-02-20 09:52:05 -0500

    Change user load

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -26,29 +26,29 @@ class Api::UsersController < ApplicationController
     @servers = current_user.servers.includes(:channels, :audio_channels)
     @channels = @servers.map(&:channels).flatten.uniq
     @audio_channels = @servers.map(&:audio_channels).flatten.uniq
-    @requests = FriendRequest.where(friend_id: current_user.id).or(FriendRequest.where(user_id: current_user_id))
+    @requests = FriendRequest.where(friend_id: current_user_id).or(FriendRequest.where(user_id: current_user_id))
     @friendships = current_user.friendships.pluck(:friend_id)
     @dm_channels = current_user.dm_channels
 
     # This is the code I would actually use; however, to decrease subsequent load times for non-tech people viewing my
     # site, I have temporarily replaced it with a load of all the users.
-    dm_user_ids = [current_user_id]
+    # dm_user_ids = [current_user_id]
 
-    @dm_channels.each do |channel|
-      dm_arr = channel.name.split('-')
-      if dm_arr[0].to_i == current_user_id
-        dm_user_ids << dm_arr[1].to_i
-      else
-        dm_user_ids << dm_arr[0].to_i
-      end
-    end
+    # @dm_channels.each do |channel|
+    #   dm_arr = channel.name.split('-')
+    #   if dm_arr[0].to_i == current_user_id
+    #     dm_user_ids << dm_arr[1].to_i
+    #   else
+    #     dm_user_ids << dm_arr[0].to_i
+    #   end
+    # end
     
-    @users = User.distinct.select('users.*').left_outer_joins(:friend_requests)
-      .left_outer_joins(:incoming_friend_requests).left_outer_joins(:friendships)
-      .where("incoming_friend_requests_users.user_id = :current_user_id OR friend_requests.friend_id = :current_user_id OR friendships.friend_id = :current_user_id OR users.id IN (:dm_user_ids)", current_user_id: current_user_id, dm_user_ids: dm_user_ids)
-      .includes(:sessions, :server_memberships)
+    # @users = User.distinct.select('users.*').left_outer_joins(:friend_requests)
+    #   .left_outer_joins(:incoming_friend_requests).left_outer_joins(:friendships)
+    #   .where("incoming_friend_requests_users.user_id = :current_user_id OR friend_requests.friend_id = :current_user_id OR friendships.friend_id = :current_user_id OR users.id IN (:dm_user_ids)", current_user_id: current_user_id, dm_user_ids: dm_user_ids)
+    #   .includes(:sessions, :server_memberships)
     
-    # @users = User.all.includes(:sessions, :server_memberships)
+    @users = User.all.includes(:sessions, :server_memberships)
     render "api/users/user_data"
   end
 
