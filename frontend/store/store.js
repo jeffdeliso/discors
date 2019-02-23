c629756b8b5f544eb00aaf9edf8f08629208a9e5
Jeff DeLiso <jeff.deliso@gmail.com>
2019-02-23 15:42:18 -0500

    Fix word wrap

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
@@ -7,8 +7,8 @@ const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
-    // applyMiddleware(thunk)
+    // applyMiddleware(thunk, logger)
+    applyMiddleware(thunk)
   )
 );
 
